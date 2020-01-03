0e4709ac63e749cdd15f72105ae04dcbaadaf0dd
Enno Boland <g@s01.de>
2020-01-03 16:54:48 +0100

    test: use macro do describe simple input/output tests from the fuzzer.

--- test/common.h
+++ test/common.h
@@ -9,5 +9,15 @@
 #define COMMON_H
 
 #define NSON_P(s) s, strlen(s)
+#define INPUT_CHECK(name, parser, ...) static void \
+name() { \
+	char *result = NULL; \
+	const char input[] =  __VA_ARGS__; \
+	Nson nson; \
+	nson_parse_ ## parser (&nson, input, sizeof(input)); \
+	nson_to_ ## parser(&nson, &result); \
+	nson_clean(&nson); \
+	free(result); \
+}
 
 #endif /* !COMMON_H */
